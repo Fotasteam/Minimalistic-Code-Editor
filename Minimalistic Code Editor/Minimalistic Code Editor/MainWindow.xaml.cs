3272f6abaa1c0f9f8d14d9a7d1b83b9b693cbac1
Fota Steam <fotasteam@gmail.com>
2023-03-07 19:13:58 +0100

    experimenting with the titlebar

--- Minimalistic Code Editor/Minimalistic Code Editor/MainWindow.xaml.cs
+++ Minimalistic Code Editor/Minimalistic Code Editor/MainWindow.xaml.cs
@@ -19,6 +19,9 @@ using Windows.Foundation.Collections;
 using System.Runtime.InteropServices;
 using WinRT;
 using CommunityToolkit;
+using Microsoft.UI.Windowing;
+using Microsoft.UI;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -28,14 +31,42 @@ namespace Minimalistic_Code_Editor
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
+
     public sealed partial class MainWindow : Window
     {
+        private Microsoft.UI.Windowing.AppWindow m_AppWindow;
+
+        private Microsoft.UI.Windowing.AppWindow GetAppWindowForCurrentWindow()
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return Microsoft.UI.Windowing.AppWindow.GetFromWindowId(wndId);
+        }
+
         public MainWindow()
         {
             this.InitializeComponent();
 
-            ExtendsContentIntoTitleBar = true;
-            SetTitleBar(AppTitleBar);
+            //ExtendsContentIntoTitleBar = true;
+            //SetTitleBar(AppTitleBar);
+
+            m_AppWindow = GetAppWindowForCurrentWindow();
+            // Check to see if customization is supported.
+            // Currently only supported on Windows 11.
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+                var titleBar = m_AppWindow.TitleBar;
+                //titleBar.SetDragRectangles();
+                // Hide default title bar.
+                titleBar.ExtendsContentIntoTitleBar = true;
+            }
+            else
+            {
+                // Title bar customization using these APIs is currently
+                // supported only on Windows 11. In other cases, hide
+                // the custom title bar element.
+                AppTitleBar.Visibility = Visibility.Collapsed;
+            }
         }
 
     }
